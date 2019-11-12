4854a8e546a730eec73ff253c6f3736c07f43232
Jerry Liu <jerry.liu@hotmail.se>
2019-11-12 13:23:23 +0100

    Refactored

--- scripts/ElasticQuery.py
+++ scripts/ElasticQuery.py
@@ -38,57 +38,51 @@ ul_logger.propagate = False
 
 
 class ElasticQuery(object):
-    def __init__(self, server_address, index, username, password):
+    def __init__(self, es_server, es_index, username, password):
         """
         ElasticQuery object for querying dataframes from server
 
-        :param server_address: es server addr
+        :param es_server: es server addr
         :param index: es index on server
         """
-        self.index = index
+        self.QUERY_SIZE = 10000
+        self.es_index = es_index
+        self.es_server = es_server
+
         try:
             logger.debug('Initializing connection.')
-            self.client = Elasticsearch(server_address, http_auth=(username, password), timeout=30000)
+            self.client = Elasticsearch(self.es_server, http_auth=(username, password), timeout=30000)
             self.client.info()
         except exceptions.AuthenticationException as e:
             logger.error('Client Authorization Failed.')
             raise e
 
-    def query_time(self, start_time: datetime, window_time=5, save_data=''):
+        # Columns of interest
+        self.col_flow = ['src_addr', 'src_port', 'dst_addr', 'dst_port', 'ip_protocol', 'packets', 'bytes']
+        self.col_netflow = ['first_switched', 'last_switched']
+        self.col_node = ['ipaddr']
+        self.columns = self.col_flow + self.col_netflow + self.col_node
+
+        self.response_columns = ['hits.hits._source.flow.' + _ for _ in self.col_flow] + \
+                                ['hits.hits._source.netflow.' + _ for _ in self.col_netflow] + \
+                                ['hits.hits._source.node.' + _ for _ in self.col_node]
+        self.response_filter = ['_scroll_id', 'hits.total.value', 'hits.hits._source.@timestamp'] + self.response_columns
+
+    def query_time(self, start_time: datetime, window_time=5):
         """
         Queries ElasticSearch server starting at start_time
 
         :param start_time: datetime to start search at
         :param window_time: time window size of search
-        :param save_data: file name to save data
+        :param save_data: file path to save data
 
-        :return: dataframe containing data in the time window
+        :return: dataframe containing data in the time window if any
         """
-        # Query parameters
-        query_index = self.index
-        query_size = 10000
-
-        # save_file parameters
-        if save_data:
-            data_folder = os.getcwd()  # Configure
-            fp_data = os.path.join(data_folder, save_data)
-
         # Time parameters
         time_current = start_time
         time_change = timedelta(minutes=window_time)
 
-        # Query parameters
-        columns1 = ['src_addr', 'src_port', 'dst_addr', 'dst_port', 'ip_protocol', 'packets', 'bytes']
-        columns2 = ['first_switched', 'last_switched']
-        columns3 = ['ipaddr']
-        columns = columns1 + columns2 + columns3
-
-        r_columns = ['hits.hits._source.flow.' + _ for _ in columns1] + \
-                    ['hits.hits._source.netflow.' + _ for _ in columns2] + \
-                    ['hits.hits._source.node.' + _ for _ in columns3]
-        r_filter = ['_scroll_id', 'hits.total.value', 'hits.hits._source.@timestamp'] + r_columns
-
-        query_filter = \
+        query = \
             {'query':
                  {'bool':
                       {'filter':
@@ -103,19 +97,31 @@ class ElasticQuery(object):
              }
 
         logger.debug('Querying time %s' % time_current.isoformat())
-        response = self.client.search(index=query_index,
-                                      body=query_filter,
-                                      size=query_size,
-                                      scroll='2m',
-                                      filter_path=r_filter)
+        return self._run_query(query)
 
+    def _run_query(self, query):
+        """
+        Queries ElasticSearch server with given query body, saves result to file if a path is given
+
+        :param query: query body given to elastic search
+        :return: results as a dataframe
+        """
+        response = self.client.search(index=self.es_index,
+                                      body=query,
+                                      size=self.QUERY_SIZE,
+                                      scroll='2m',
+                                      filter_path=self.response_filter)
         scroll_id = response['_scroll_id']
+        n_flows = response['hits']['total']['value']
+        if n_flows == 0:
+            raise exceptions.NotFoundError()
 
         # Process batches
-        logger.debug('Processing %i flows.' % response['hits']['total']['value'])
+        logger.debug('Processing %i flows.' % n_flows)
+
+        df_tmp = pd.DataFrame(columns=self.columns)
 
         response_batch = 1
-        df_tmp = pd.DataFrame(columns=columns)
         while True:
             rows = []
             for hit in response['hits']['hits']:
@@ -127,22 +133,19 @@ class ElasticQuery(object):
             df_tmp = df_tmp.append(pd.DataFrame.from_dict(rows), sort=False)
 
             # Exit condition
-            if len(response['hits']['hits']) < query_size:
+            if len(response['hits']['hits']) < self.QUERY_SIZE:
                 logger.debug('Processed %i batches.' % response_batch)
                 break
 
             # Get next set
-            response = self.client.scroll(scroll_id=scroll_id, scroll='2m', filter_path=r_filter)
+            response = self.client.scroll(scroll_id=scroll_id, scroll='2m', filter_path=self.response_filter)
             response_batch += 1
-
-        if save_data:
-            df_tmp.to_csv(fp_data)
 
         return df_tmp
 
 
 if __name__ == '__main__':
     eq = ElasticQuery(server, index, username, password)
-    # df = eq.query_time(datetime(2019, 9, 2, 9, 0))
-    df = eq.query_time(datetime(2019, 10, 28, 9, 0))
+    df = eq.query_time(datetime(2019, 9, 2, 9, 0))
+    # df = eq.query_time(datetime(2019, 10, 28, 9, 0))
 
