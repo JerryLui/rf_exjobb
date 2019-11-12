bfc618071cdff3be9e719a954404e5fcea383fbb
Martin Eriksson <60point3@gmail.com>
2019-11-12 14:30:07 +0100

    Detector is improving, produces histograms and divergences. Will implement MAV-detection

--- scripts/detector.py
+++ scripts/detector.py
@@ -36,6 +36,15 @@ class DetectorPool():
     def __init__():
         pass
 
+    def add_detector():
+        pass
+
+    def next_timestep():
+        pass
+
+    def get_results():
+        pass
+
 class Detector():
     '''
     A detector which takes a dataframe and filters it, then runs the
@@ -48,8 +57,9 @@ class Detector():
         self.n_bins       = 1
         self.mav_steps    = 1
         self.features     = []
-        self.aggregations = []
+        self.aggregations = [] #Feature/aggregation should probably be a pair
         self.filter       = {}
+        self.thresh       = 1
 
         #Parameter-dependent initializations
         self.seeds = np.random.choice(100000, size=self.n_seeds, replace=False)
@@ -57,8 +67,9 @@ class Detector():
 
         #Non-parameter initializations
         self.step = 0
-        self.divs = np.zeros((1, ))
-        self.mav  = np.zeros((1, ))
+        self.divs = np.zeros((len(self.features), self.n_seeds))
+        self.mav  = np.zeros((len(self.features, self.mav_steps))) #Use np.roll
+        self.last_histograms = None
 
         #Bool to signify if detector is ready for detection
         # (~mav steps have passed)
@@ -70,6 +81,9 @@ class Detector():
         '''
         frame = self.applyfilter(frame)
 
+        histograms = np.zeros((len(self.features), self.n_seeds, self.n_bins))
+        divs       = np.zeros((len(self.features), self.n_seeds)))
+
         for f, feat in enumerate(self.features):
             #AGGREGATIONS ARE NOT IMPLEMENTED YET
 
@@ -78,19 +92,34 @@ class Detector():
             for s, seed in enumerate(self.seeds):
                 unique, counts = np.unique(feat_series, return_counts=True)
 
-                for uni, cnt in zip(unique, counts):
-                    #Hash features (And save?)
-                    #Add to histogram (Vectorize?)
-                    pass
+                bins = hash_to_buckets(unique, self.bucket_limits, seed)
+
+                for b, cnt in zip(bins, counts):
+                    histograms[f, s, b] += cnt
+
+                #Calculate divergence, will be used in detection step
+                if self.last_histograms is not None:
+                    div = KL_divergence(histograms[f, s, :],
+                            self.last_histograms[f, s, :])
+                    divs[f, s] = div
 
             #Detection per seed, use mav method
+            #What do we need?
+            # - Mav array             -> rolling numpy array
+            # - Last iteration's bins -> last_histograms
             for s, seed in enumerate(self.seeds):
                 pass
 
             #Extraction must also be done on this level
             #Check number of detected seeds
 
+        if step < self.mav_steps:
+            self.mav[f, step] = None
+        else:
+            self.mav[f, step] = None
+
         step += 1
+        self.last_histograms = histograms
 
     def applyfilter(self, frame):
         pass
