64c74880b17d3df51ac414be215d846c629b93bf
Jerry Liu <jerry.liu@recordedfuture.com>
2019-11-21 09:24:01 +0000

    TypeError fix

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -106,7 +106,7 @@ def run(start_time: datetime, end_time: datetime, window_size: timedelta):
         results = dp.run_next_timestep(future.result())
         detections.append(results[0])
         detection_frames.append(results[1])
-        logger.debug(' '.join([len(_) for _ in results]))
+        logger.debug(' '.join([str(len(_)) for _ in results]))
 
     full_detections = pd.concat(detection_frames)
     pd.save(full_detections, 'output/detection_frame.pkl')
